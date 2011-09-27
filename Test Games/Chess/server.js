141e95d4414b314d84d8f39de999d9d072b194a9
Ryan Kohler <ryan.c.kohler@gmail.com>
2011-09-27 03:53:03 -0700

    Adding in test games

---
+++ Test Games/Chess/server.js
@@ -0,0 +1,47 @@
+var sys = require('sys');
+var express = require('express');
+
+var app = express.createServer();
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
+});
+
+app.get('/',function(req,res){
+});
+
+app.post('/setup.json',function(req,res){
+  sys.log('body:'+sys.inspect(req.body));
+  result = null;
+  switch(req.body.type){
+    case 'game':
+      result = {'status':'form','data':{'gamename':{'label':'Game Name','type':'string'},'description':{'label':'Description','type':'string'}}};
+    break;
+    
+    case 'gameform':
+      req.body.data = JSON.parse(req.body.data);
+      result = {'status':'game','data':{'name':req.body.data.gamename,'description':req.body.data.description,'lists':[{'name':'white','count':1},{'name':'black','count':1}],'start':'white'}};
+    break;
+    
+    case 'join':
+      result ={'status':'join','data':{}};
+    break;
+    
+    case 'joinform':
+    break;
+    
+    case 'start':
+      result = {'status':'start','data':{'url':'http://127.0.0.1:8125/game'}};
+    break;
+  }
+  if(result != null){
+    result = JSON.stringify(result);
+    sys.log("Return Value: "+result);
+    res.send(result);
+  }else{
+    sys.log("No return value");
+  }
+});
+
+app.listen(8126);
