eb7a0e17760445d6301a7aeb4df407a914459c2d
Richard Penwell <almightylinuxgod@me.com>
2011-09-30 21:28:56 -0700

    FEATURE: User should be automatically logged in after creating the user
    Many websites automatically log the user in after the user is created.
    This change emulates that behavior.  This is accomplished by setting up
    the session after a successful user creation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, :notice => "Signed Up!"
     else
       render "new"
